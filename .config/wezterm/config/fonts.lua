1f2959bada27ca0b06fe10327bc30f7ddfe7766b
Howl-24 <howell.ding@outlook.com>
2025-03-16 22:34:41 +0800

    change font

--- .config/wezterm/config/fonts.lua
+++ .config/wezterm/config/fonts.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'MesloLGL Nerd Font'
+local font_family = 'MesloLGM Nerd Font'
 local font_size = 12
 
 return {
